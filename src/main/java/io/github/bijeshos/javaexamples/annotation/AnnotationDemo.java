d2baa6886edd6054a4e63a97b24849dbef120f7f
Bijesh O S <bijeshos@users.noreply.github.com>
2020-01-16 11:07:48 +0530

    added initial version of annotation demo

---
+++ src/main/java/io/github/bijeshos/javaexamples/annotation/AnnotationDemo.java
@@ -0,0 +1,8 @@
+package io.github.bijeshos.javaexamples.annotation;
+
+@MyAnnotation(comment = "annotation demo")
+public class AnnotationDemo {
+    public static void main(String[] args) {
+        System.out.println("Hi from annotation demo");
+    }
+}
---
+++ src/main/java/io/github/bijeshos/javaexamples/annotation/MyAnnotation.java
@@ -0,0 +1,13 @@
+package io.github.bijeshos.javaexamples.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+//@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface MyAnnotation {
+    public String comment();
+}
